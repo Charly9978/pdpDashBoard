1b9bd5c6d5acd7782f7f2ccaca20aaae430ae8ca
benj <diebenjamin@yahoo.com>
2020-09-22 21:56:37 +0200

    mise en place du cors

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,11 +1,11 @@
 export default function ({ $axios }, inject) {
     // Create a custom axios instance
     const api = $axios.create({
-      url:'http://localhost:1337/graphql',
+      baseURL:'http://localhost:1337/graphql',
       method: 'post',
       transformResponse: [(data)=>{
-        const result = JSON.parse(data).data
-        return result;
+        
+        return data.data;
       }],
     })
   
