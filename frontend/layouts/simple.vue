9a8232528c710b51f28b91cca5b7ee79cc0d92ce
benj <diebenjamin@yahoo.com>
2020-09-25 11:49:20 +0200

    amélioration de la page logout

--- frontend/layouts/simple.vue
+++ frontend/layouts/simple.vue
@@ -1,9 +1,7 @@
 <template>
   <v-app dark>
     <v-main>
-      <v-container>
         <nuxt />
-      </v-container>
     </v-main>
   </v-app>
 </template>
--- frontend/pages/logout.vue
+++ frontend/pages/logout.vue
@@ -1,7 +1,36 @@
 <template>
-  <div>
-      Logout
-  </div>
+  <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Suivi des plans de prévention</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <span>Vous n'êtes plus connecté à cette application. Cliquez sur le bouton de connexion pour accéder à l'application</span>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" to="/">Connexion</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
 </template>
 
 <script>
