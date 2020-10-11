e50ee4e947741f51e8a433c720e7658bf19a6297
benj <diebenjamin@yahoo.com>
2020-10-11 21:16:56 +0200

    modif query pour récupérer plsu d'infos sur les pdps par entreprise

--- frontend/graphql/queries/entreprise/entreprisesQuery.js
+++ frontend/graphql/queries/entreprise/entreprisesQuery.js
@@ -1,14 +1,15 @@
 export const entreprisesQuery = {
-    query:`query{
-        entreprises(sort:"nom:asc"){
+    query:`query {
+      entreprises {
+        id
+        nom
+        adresse
+        contact
+        principal_activity
+        plan_de_preventions {
           id
-          nom
-          adresse
-          contact
-          principal_activity
-          pdpEnCours: plan_de_preventions(where:{Archiver:false}){
-            id
-          }
+          Archiver
         }
-      }`
+      }
+    }`
 }
