7e024fcdfb196ed6723ed5ad61ef7755a956418a
benj <diebenjamin@yahoo.com>
2020-10-11 21:33:58 +0200

    demande de confirmation avant suppression d'une entreprise

--- frontend/components/entreprise/entrepriseCard.vue
+++ frontend/components/entreprise/entrepriseCard.vue
@@ -1,4 +1,6 @@
 <template>
+<div>
+    <validation :text="validationText" :openValidation="openValidation" @yes="deleteEntreprise" @no="openValidation = false"></validation>
 <v-card :color='!isPdpEnCours?"grey lighten-3":"white"'>
     <v-card-title primary-title class="test">
         {{entreprise.nom}}
@@ -6,7 +8,7 @@
             <div  v-if="!isPdpEnCours" class="text-subtitle-1">Pas de plans de prévention en cours</div>
         <v-spacer></v-spacer>
         <v-card-actions>
-        <v-btn v-if="$auth.isAdmin && !isPdp" rounded color="primary" icon dark>
+        <v-btn v-if="$auth.isAdmin && !isPdp" rounded color="primary" icon dark @click="openValidation = true">
             <v-icon>mdi-delete</v-icon>
         </v-btn>
         <v-btn rounded color="primary" icon dark nuxt :to="`/entreprise/${entreprise.id}`">
@@ -16,11 +18,19 @@
     </v-card-title>
 
     <v-card-subtitle>{{entreprise.principal_activity}}</v-card-subtitle>
-</v-card>  
+</v-card> 
+</div> 
 </template>
 
 <script>
+import validation from '~/components/utilities/Validation'
+
 export default {
+
+    components:{
+        validation
+    },
+
     props:{
         entreprise:{
             type: Object,
@@ -30,7 +40,9 @@ export default {
     data(){
         return{
             isPdpEnCours:this.entreprise.plan_de_preventions.filter(pdp=>pdp.Archiver == false).length>0?true:false,
-            isPdp: this.entreprise.plan_de_preventions.length>0?true:false
+            isPdp: this.entreprise.plan_de_preventions.length>0?true:false,
+            validationText:"Etes vous sûr de vouloir supprimer cette entreprise? Cette action est définitive!",
+            openValidation:false
         }
     }
 
