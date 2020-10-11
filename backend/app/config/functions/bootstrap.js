403aba06fa9e86aa59223b4e922b96e4a9a909dc
benj <diebenjamin@yahoo.com>
2020-10-11 17:48:46 +0200

    creation de la fonction d'import des datas

--- backend/app/config/functions/bootstrap.js
+++ backend/app/config/functions/bootstrap.js
@@ -9,5 +9,31 @@
  *
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
+const fs = require('fs')
 
-module.exports = () => {};
+module.exports = () => {
+
+    //mettre les fichiers json dans un dossier data dans app. 
+    //les noms des fichiers doivent correspondre au nom des collections
+    //modifier la variable d'environnement IMPORTDATA = "true" type string
+    //redemarrer le docker
+    //une fois l'importation réussi arreté le docker, changer la variable à "false" et redémarrer le docker
+
+    const importDatas = () => {
+     
+        const files =  fs.readdirSync('./data')
+    
+        files.forEach( file => {
+             const data = require(`../../data/${file}`)
+             const fileWithoutJson = file.split('.json')[0]
+             data.forEach( async (item,index) => {
+                 try {
+                     await strapi.services[fileWithoutJson].create(item)
+                 } catch (error) {
+                     console.log(`error from item ${index}`,item,error)
+                 }
+                 })
+             })
+    }
+
+};
